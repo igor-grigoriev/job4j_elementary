d56bda1f0ea5b9d01b78b3466dadd5c8578a61ca
igor-grigoriev <grigoriev.i.v@mail.ru>
2022-07-04 09:21:54 +0300

    1. Модульные тесты. [#159435] fix

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -16,8 +16,8 @@ public class ConverterTest {
 
     @Test
     public void whenConvert200RblThen3dot3333Dlr() {
-        float in = 180;
-        float expected = 3;
+        float in = 200;
+        float expected = 3.3333f;
         float out = Converter.rubleToDollar(in);
         float eps = 0.0001f;
         Assert.assertEquals(expected, out, eps);
