2ac8820a2cf39e8e32806a8ca716e7c95db5a157
igor-grigoriev <grigoriev.i.v@mail.ru>
2022-08-12 20:44:47 +0300

    5.7. Ипотека [#156315 #331164] fix

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,9 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double credit = amount * (1 + percent / 100);
-        while (credit > 0) {
-            credit = (credit - salary) * (1 + percent / 100);
+        while (amount > 0) {
+            amount = amount * (1 + percent / 100) - salary;
             year++;
         }
         return year;
